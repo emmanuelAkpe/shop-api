520bd68b88a4c4c6a33751b3a8d28bfb102c3941
Emmanuel Akpe <emmanuelakpe1@gmail.com>
2022-04-21 14:01:07 +0000

    authentication

---
+++ controllers/auth.controller.js
@@ -0,0 +1,59 @@
+const res = require("express/lib/response");
+const User = require("../models/user.model");
+const { registerSchema, loginSchema } = require("../utils/validation");
+const bcrypt = require("bcryptjs");
+
+const register = async (req, res) => {
+  const { value, error } = registerSchema.validate(req.body);
+  //   console.log(value);
+  if (error) {
+    return res.status(400).json(error.message);
+  }
+
+  let user = await User.findOne({ email: value.email });
+
+  if (user) {
+    return res.status(409).json({ msg: "EMail already in use." });
+  }
+  const hashedPassword = await bcrypt.hash(value.password, 10);
+  console.log(hashedPassword);
+
+  user = await User.create({
+    username: value.username,
+    email: value.email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json(user);
+};
+
+const login = async (req, res) => {
+  // validate user input
+  const { value, error } = loginSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json(error.message);
+  }
+  // check if user is in the database
+  let user = await User.findOne({ email: value.email });
+
+  // if user is not found
+  if (!user) {
+    return res.status(400).json({ msg: "invalid credentials" });
+  }
+
+  //   compare password with the stored password
+
+  const isMatch = await bcrypt.compare(value.password, user.password);
+
+  //   if passwords do not match
+  if (!isMatch) {
+    return res.status(400).json({ msg: "invalid credentials" });
+  }
+  // return the user if everything works well
+  res.status(200).json(user);
+};
+
+module.exports = {
+  register,
+  login,
+};
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const {dbConnect} = require("./config/db.Connect")
-const productRouter = require("./routes/products.router")
+const express = require("express");
+const app = express();
+const port = 3000;
+const { dbConnect } = require("./config/db.Connect");
+const productRouter = require("./routes/products.router");
+const authRouter = require("./routes/auth.router");
+app.use(express.json());
 
+app.use("/products", productRouter);
+app.use("/auth", authRouter);
 
-app.use("/products", productRouter)
+const start = async () => {
+  await dbConnect();
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+};
 
-const start = async ()=>{
-    await dbConnect()
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-}
-
-
-start()
-
-
+start();
